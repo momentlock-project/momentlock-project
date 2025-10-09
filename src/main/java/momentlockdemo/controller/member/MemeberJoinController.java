e8900999809b1e2f3e006fb6cf54aa07953de352
minkyong00 <minkyong131@gmail.com>
2025-10-09 19:17:54 +0900

    회원가입 컨트롤러 post 매핑, 이메일, 비밀번호 유효성 검사 추가

--- src/main/java/momentlockdemo/controller/member/MemeberJoinController.java
+++ src/main/java/momentlockdemo/controller/member/MemeberJoinController.java
@@ -1,18 +1,46 @@
 package momentlockdemo.controller.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import momentlockdemo.dto.MemberDto;
+import momentlockdemo.entity.Member;
+import momentlockdemo.service.MemberService;
 
 @Controller("MemberJoinController")
 @RequestMapping("/momentlock")
 public class MemeberJoinController {
 	
+	@Autowired
+	private MemberService memberService;
+	
 	// 회원가입
 	@GetMapping("/memberjoin")
-	public String memberjoinPage() {
+	public String memberjoinPage(Model model) {
+		model.addAttribute("memberDto", new MemberDto());
 		return "html/member/memberJoin";
 	}	
 	
+	
+	@PostMapping("/join")
+	public String memberjoin(@ModelAttribute("memberDto") MemberDto memberDto) {
+		
+		Member member = Member.builder()
+				.username(memberDto.getUsername())
+				.name(memberDto.getName())
+				.password(memberDto.getPassword())
+				.nickname(memberDto.getNickname())
+				.phonenumber(memberDto.getPhonenumber())
+				.build();
+		
+		memberService.createMember(member);
+		
+		return "html/main";
+	}
 
 }
