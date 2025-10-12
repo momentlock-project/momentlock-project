3a9b44b4d948f448d5c1537ffa426b76e9c83134
minkyong00 <minkyong131@gmail.com>
2025-10-12 14:48:11 +0900

    비밀번호 재설정 유효성 검사 passwordreset.js 추가

--- src/main/resources/static/js/member/passwordreset.js
+++ src/main/resources/static/js/member/passwordreset.js
@@ -6,13 +6,6 @@ const passwordconfirm = document.querySelector("input[name='passwordconfirm']");
 const passwordP = document.querySelector(".msg.password");
 const passwordconfirmP = document.querySelector(".msg.passwordconfirm");
 
-// username(이메일) 유효성 검사
-validateInput(
-	document.querySelector("input[name='username']"), 
-	document.querySelector(".msg.username"),
-	/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 
-	"이메일 형식에 맞게 입력해주세요."
-);
 
 // 비밀번호 유효성 검사
 // 영대소문자, 숫자, 특수문자 포함 8-20자 
@@ -23,14 +16,6 @@ validateInput(
 	"영대소문자, 숫자, 특수기호 포함 8-20자를 입력하세요."
 );
 
-// 전화번호 유효성 검사
-// 하이픈없이 숫자 11자
-validateInput(
-	document.querySelector("input[name='phonenumber']"), 
-	document.querySelector(".msg.phonenumber"), 
-	/^\d{11}$/, 
-	"하이픈(-)없이 전화번호 11자리를 입력하세요."
-);
 
 // 비밀번호 일치 확인
 passwordconfirm.addEventListener("input", () => {
@@ -40,31 +25,10 @@ passwordconfirm.addEventListener("input", () => {
 	}
 })
 
-
-const form = document.querySelector("#memberjoin");
+const form = document.querySelector("#passwordreset");
 
 form.addEventListener("submit", e => {
 	e.preventDefault(); // 기본 submit 막기
-	alert("회원가입이 완료되었습니다!");
+	alert("비밀번호 재설정되었습니다!");
 	form.submit();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
