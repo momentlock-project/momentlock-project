b108d0f8d1b85406587095336e7795ddde0e89f5
CTK <tesold7467@gmail.com>
2025-10-14 15:52:51 +0900

    공지사항 레파짓토리 서비스인플 추가

---
+++ src/main/java/momentlockdemo/service/impl/NoticeQaServiceImpl.java
@@ -0,0 +1,44 @@
+package momentlockdemo.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service; 
+import org.springframework.transaction.annotation.Transactional; 
+
+import momentlockdemo.entity.master.NoticeQa;
+import momentlockdemo.repository.NoticeQaRepository;
+import momentlockdemo.service.NoticeQaService;
+
+@Service 
+public class NoticeQaServiceImpl implements NoticeQaService {
+	
+	@Autowired
+	private NoticeQaRepository noticeQaRepository;
+	
+	@Override
+	@Transactional(readOnly = true) 
+	public List<NoticeQa> getAllNoticeQa() {
+		return noticeQaRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<NoticeQa> getNoticeQaById(Long id) {
+		return noticeQaRepository.findById(id);
+	}
+	
+	@Override
+	@Transactional
+	public NoticeQa insertNoticeQa(NoticeQa noticeQa) {
+		return noticeQaRepository.save(noticeQa);
+	}
+	
+	@Override
+	@Transactional
+	public void deleteNoticeQa(Long id) {
+		noticeQaRepository.deleteById(id);
+	}
+}
