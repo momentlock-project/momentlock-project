065370fbd9b267b81191abcdf2b2e78e9c021b92
minkyong00 <minkyong131@gmail.com>
2025-10-14 12:01:10 +0900

    문의게시판 날짜 형식 2025/10/13 형식으로 변경

--- src/main/resources/static/js/member/memberbox.js
+++ src/main/resources/static/js/member/memberbox.js
@@ -1,1 +1,6 @@
-document.querySelector(".col.regdate")
+// 날짜 형식 2025/10/13으로 변경
+document.querySelectorAll(".col.regdate").forEach(inquiry => {
+	inquiry.textContent
+		= inquiry.textContent.substring(
+			0, inquiry.textContent.indexOf("T")).replaceAll("-", "/");
+})
