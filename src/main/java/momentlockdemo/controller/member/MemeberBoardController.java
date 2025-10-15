130d8ac18c449a5697933039331a1d62d80f402d
minkyong00 <minkyong131@gmail.com>
2025-10-15 16:51:38 +0900

    멤버 문의게시판 상세정보 모달창 완료

--- src/main/java/momentlockdemo/controller/member/MemeberBoardController.java
+++ src/main/java/momentlockdemo/controller/member/MemeberBoardController.java
@@ -1,13 +1,19 @@
 package momentlockdemo.controller.member;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import momentlockdemo.service.DeclarationService;
 import momentlockdemo.service.InquiryService;
@@ -32,6 +38,20 @@ public class MemeberBoardController {
 			@PageableDefault(page = 0, size = 10, sort = "inqid", direction = Sort.Direction.DESC)Pageable pageable) {
 		model.addAttribute("inquirylist", inquiryService.getAllInquiries(pageable));
 		return "html/member/memberinquirylist";
+	}
+	
+	// 문의게시판 상세
+	// map 생성해서 front에서 json으로 받음
+	@GetMapping("/memberinquirylist/{inqid}")
+	public ResponseEntity<Map<String, Object>> memberinquirylistPage(@PathVariable Long inqid) {
+		return inquiryService.getInquiryById(inqid)
+				.map(inquiry -> {
+					Map<String, Object> data = new HashMap<String, Object>();
+					data.put("inqtitle", inquiry.getInqtitle());
+					data.put("inqcontent", inquiry.getInqcontent());
+					data.put("inqregdate", inquiry.getInqregdate());				
+					return ResponseEntity.ok(data);
+				}).orElse(ResponseEntity.notFound().build());
 	}
 	
 	// 신고게시판
--- src/main/resources/static/js/member/memberbox.js
+++ src/main/resources/static/js/member/memberbox.js
@@ -1,24 +1,59 @@
-// 날짜 형식 2025/10/13으로 변경
-document.querySelectorAll(".col.regdate").forEach(inquiry => {
-	inquiry.textContent
-		= inquiry.textContent.substring(
-			0, inquiry.textContent.indexOf("T")).replaceAll("-", "/");
-})
-
-const overlay = document.querySelector(".modal-overlay");
-const modal = document.querySelector(".box-modal");
-
-// row 하나 클릭 시 상세 정보 보여주기
-document.querySelectorAll(".row").forEach(row => {
-	row.addEventListener("click", () => {
-		modal.style.display = 'block';
-		overlay.style.display = 'block';
+document.addEventListener("DOMContentLoaded", function() {
+	
+	// 날짜 형식 2025/10/13으로 변경
+	document.querySelectorAll(".col.regdate").forEach(inquiry => {
+		inquiry.textContent
+			= inquiry.textContent.substring(
+				0, inquiry.textContent.indexOf("T")).replaceAll("-", "/");
 	})
+	
+	const overlay = document.querySelector(".modal-overlay");
+	const modal = document.querySelector(".box-modal");
+	const closebtn = document.querySelector(".close-btn");
+	
+	// row 하나 클릭 시 상세 정보 보여주기
+	document.querySelectorAll(".row").forEach(row => {
+		
+		try {
+			row.addEventListener("click", async () => {
+				const response = 
+					await fetch("/momentlock/memberinquirylist/" 
+							+ row.children[0].textContent, {
+						method: "GET"
+					})
+					
+				if(!response.ok){
+					throw new Error("상세정보를 찾을 수 없습니다");
+				}
+				
+				const data = await response.json();
+				
+				document.querySelector(".modal-title").innerHTML = data.inqtitle;
+				document.querySelector(".modal-content").innerHTML = data.inqcontent;
+				
+				if(modal && overlay){
+					// 모달, 오버레이 display block으로 변경
+					modal.style.display = 'block';
+					overlay.style.display = 'block';
+				}
+					
+			})
+		} catch(err){
+			console.log("error", err);
+		}
+	})
+	
+	// 모달창이 열려있을 경우
+	if(closebtn != null){
+		// x버튼 클릭 시 모달창 닫기
+		closebtn.addEventListener("click", () => {
+			
+			// 모달, 오버레이 display none으로 변경
+			modal.style.display = "none";
+			overlay.style.display = 'none';
+		})
+	}
+	
+	
 })
-
-
-
-document.querySelector(".close-btn").addEventListener("click", () => {
-	modal.style.display = "none";
-	overlay.style.display = 'none';
-})
+ 
