69b9287d1a17e14a359a45dcfe24c555fd837028
minkyong00 <minkyong131@gmail.com>
2025-10-15 11:20:34 +0900

    문의게시판 모달창 js 구현 완료

--- src/main/resources/static/js/member/memberbox.js
+++ src/main/resources/static/js/member/memberbox.js
@@ -5,15 +5,20 @@ document.querySelectorAll(".col.regdate").forEach(inquiry => {
 			0, inquiry.textContent.indexOf("T")).replaceAll("-", "/");
 })
 
+const overlay = document.querySelector(".modal-overlay");
 const modal = document.querySelector(".box-modal");
 
 // row 하나 클릭 시 상세 정보 보여주기
 document.querySelectorAll(".row").forEach(row => {
 	row.addEventListener("click", () => {
 		modal.style.display = 'block';
+		overlay.style.display = 'block';
 	})
 })
 
+
+
 document.querySelector(".close-btn").addEventListener("click", () => {
 	modal.style.display = "none";
+	overlay.style.display = 'none';
 })
