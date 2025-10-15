0d40a25c499294f0f443fcc3d360b4f30b82b8f3
CTK <tesold7467@gmail.com>
2025-10-15 11:08:25 +0900

    master notice pagination pach

--- src/main/java/momentlockdemo/service/NoticeQaService.java
+++ src/main/java/momentlockdemo/service/NoticeQaService.java
@@ -3,6 +3,9 @@ package momentlockdemo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import momentlockdemo.entity.master.NoticeQa;
 
 public interface NoticeQaService {
@@ -12,6 +15,8 @@ public interface NoticeQaService {
 	public abstract Optional<NoticeQa> getNoticeQaById(Long id);
 	
 	public abstract NoticeQa insertNoticeQa(NoticeQa noticeQa);
+	
+	public abstract Page<NoticeQa> getPageNoticeQa(Pageable pageable);
 	
 	public abstract void deleteNoticeQa(Long id);
 }
--- src/main/java/momentlockdemo/service/impl/NoticeQaServiceImpl.java
+++ src/main/java/momentlockdemo/service/impl/NoticeQaServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service; 
 import org.springframework.transaction.annotation.Transactional; 
@@ -34,6 +36,11 @@ public class NoticeQaServiceImpl implements NoticeQaService {
 	@Transactional
 	public NoticeQa insertNoticeQa(NoticeQa noticeQa) {
 		return noticeQaRepository.save(noticeQa);
+	}
+	
+	@Override
+	public Page<NoticeQa> getPageNoticeQa(Pageable pageable) {
+		return noticeQaRepository.findAll(pageable);
 	}
 	
 	@Override
