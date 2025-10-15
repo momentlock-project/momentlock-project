a5043f16a3e53e5770e6f658c2bed1ebc83c583e
minkyong00 <minkyong131@gmail.com>
2025-10-15 10:00:49 +0900

    문의사항 상세정보 모달창 구현중

--- src/main/resources/static/js/member/memberbox.js
+++ src/main/resources/static/js/member/memberbox.js
@@ -5,4 +5,15 @@ document.querySelectorAll(".col.regdate").forEach(inquiry => {
 			0, inquiry.textContent.indexOf("T")).replaceAll("-", "/");
 })
 
+const modal = document.querySelector(".box-modal");
+
 // row 하나 클릭 시 상세 정보 보여주기
+document.querySelectorAll(".row").forEach(row => {
+	row.addEventListener("click", () => {
+		modal.style.display = 'block';
+	})
+})
+
+document.querySelector(".close-btn").addEventListener("click", () => {
+	modal.style.display = "none";
+})
