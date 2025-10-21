22377686e4e8e6d9f650537dffb3020b05a6ca80
minkyong00 <minkyong131@gmail.com>
2025-10-21 19:05:43 +0900

    나의 상자 리스트에서 상자를 만든 회원이 아닌 경우 수정, 상자 보내기 버튼 비활성화

---
+++ src/main/java/momentlockdemo/dto/MemberBoxDto.java
@@ -0,0 +1,22 @@
+package momentlockdemo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberBoxDto {
+	
+	private Long boxid; 
+
+	private String boxname;
+	
+	private LocalDateTime boxopendate;
+	
+	private String code;
+
+}
